7aebe0042d2f9bfccb542c9a995fbd7e58b546ca
sspanak <doftor.livain@gmail.com>
2026-08-22 16:01:45 +0300

    fixed some edge-case bugs

--- app/src/main/java/com/sidephone/snake/settings/GameplaySettings.kt
+++ app/src/main/java/com/sidephone/snake/settings/GameplaySettings.kt
@@ -4,5 +4,5 @@ object GameplaySettings {
 	const val TARGET_FPS = 30 // Rendering frames per second. Use multiples of the device screen refresh rate for better performance
 	const val TARGET_IPS = 15 // Advance game logic N iterations per second. Indirectly affects the FPS
 	const val GAME_SPEED = 50 // %
-	const val FOOD_STAYS_UNTIL_EATEN = false // If true, food will stay on the screen until eaten. If false, food will disappear after a while
+	const val FOOD_STAYS_UNTIL_EATEN = false
 }
